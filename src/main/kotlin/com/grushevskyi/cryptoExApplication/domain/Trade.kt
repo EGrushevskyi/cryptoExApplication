55b2daa109b9913a202e42e71bf1ea1feef350e8
Eugene Grushevskyi <e.o.grushevsky@gmail.com>
2021-11-18 00:10:37 +0100

    18.11 update

---
+++ src/main/kotlin/com/grushevskyi/cryptoExApplication/domain/Trade.kt
@@ -0,0 +1,32 @@
+package com.grushevskyi.cryptoExApplication.domain
+
+import lombok.Data
+import lombok.NoArgsConstructor
+import lombok.NonNull
+import javax.persistence.Id
+
+@Data
+@NoArgsConstructor
+class Trade {
+
+    @Id
+    var id: String? = null
+    var timestamp: Long = 0
+    var amount = 0f
+    var price = 0f
+
+    @NonNull
+    var currency: String? = null
+
+    fun Trade(
+        timestamp: Long,
+        amount: Float,
+        price: Float,
+        currency: String?
+    ) {
+        this.currency = currency
+        this.timestamp = timestamp
+        this.amount = amount
+        this.price = price
+    }
+}
